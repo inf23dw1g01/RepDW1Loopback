9a8bb4aafb774685aac3b6d8d1f2b24859c97586
Diogo <110359163+Goncalves745@users.noreply.github.com>
2024-01-15 15:02:07 +0000

    Alterado ficheiro sql e dockerfile

--- db.sql
+++ db.sql
@@ -1,27 +1,7 @@
--- MySQL dump 10.13  Distrib 8.0.34, for macos13 (arm64)
---
--- Host: 127.0.0.1    Database: Filmes
--- ------------------------------------------------------
--- Server version	5.7.44
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `Atores`
---
 
 DROP TABLE IF EXISTS `Atores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `Atores` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(512) NOT NULL,
@@ -29,48 +9,31 @@ CREATE TABLE `Atores` (
   `filmeId` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `Atores`
---
 
 LOCK TABLES `Atores` WRITE;
-/*!40000 ALTER TABLE `Atores` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Atores` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `Diretores`
---
 
 DROP TABLE IF EXISTS `Diretores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `Diretores` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(512) NOT NULL,
   `date_of_birth` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `Diretores`
---
 
 LOCK TABLES `Diretores` WRITE;
-/*!40000 ALTER TABLE `Diretores` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Diretores` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `Filme`
---
+
 
 DROP TABLE IF EXISTS `Filme`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `Filme` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(512) NOT NULL,
@@ -79,48 +42,35 @@ CREATE TABLE `Filme` (
   `diretoresId` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `Filme`
---
 
 LOCK TABLES `Filme` WRITE;
-/*!40000 ALTER TABLE `Filme` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Filme` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `Personagens`
---
+
 
 DROP TABLE IF EXISTS `Personagens`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `Personagens` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(512) NOT NULL,
   `role` varchar(512) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `Personagens`
---
 
 LOCK TABLES `Personagens` WRITE;
-/*!40000 ALTER TABLE `Personagens` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Personagens` ENABLE KEYS */;
+
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-01-12 15:39:19
+-- Adiciona valor em filmes
+INSERT INTO Filme (name, description, ano) VALUES ('Star Wars: Revenge Of The Sith', 'Plot. Orbiting above Coruscant, Obi-Wan Kenobi and Anakin Skywalker lead a mission to rescue Supreme Chancellor Palpatine from the cyborg Separatist commander General Grievous. After infiltrating Grievous'' flagship, Obi-Wan and Anakin battle the Sith Lord Count Dooku, whom Anakin decapitates.', '2005');
+INSERT INTO Filme (name, description, ano) VALUES ('Cars', 'Ao viajar para a Califórnia, o famoso carro de corridas Relâmpago McQueen se perde e vai parar em Radiator Springs, uma cidadezinha na Rota 66. Ele conhece novos amigos e aprende lições que mudam sua forma de encarar a vida.', '2006');
+INSERT INTO Diretores (name, date_of_birth) VALUES ('Quentin Tarantino', '1963');
+INSERT INTO Diretores (name, date_of_birth) VALUES ('John Krasinski', '1979');
+INSERT INTO Atores (name, date_of_birth) VALUES ('Paul Walker', '1973');
+INSERT INTO Atores (name, date_of_birth) VALUES ('Owen Wilson', '1968');
+INSERT INTO Personagens (name, role) VALUES ('Otavio', 'Leading Director');
+INSERT INTO Personagens (name, role) VALUES ('Diogo', 'Actor');
+INSERT INTO Personagens (name, role) VALUES ('Otavio', 'Secondary actor');
