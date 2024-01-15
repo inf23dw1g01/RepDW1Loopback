723ec9fb2e0e4a25a25f8ccf68dc5a1c94f18874
Goncalo Ribeiro <136264767+GonCarRib@users.noreply.github.com>
2024-01-15 19:31:44 +0000

    Update App.js

--- filmes_react_app/src/App.js
+++ filmes_react_app/src/App.js
@@ -1,4 +1,4 @@
-import { Admin, Resource } from "react-admin";
+import { Admin, Resource ,EditGuesser} from "react-admin";
 import filmeProvider from "react-admin-lb4";
 import './App.css';
 import { AtoresList } from "./AtoresList";
@@ -11,10 +11,10 @@ const dataProvider = filmeProvider("http://localhost:8080");
 
 const App = () => (
     <Admin dataProvider={dataProvider}>
-        <Resource name="filmes" list={FilmesList} />
-        <Resource name="atores" list={AtoresList} />
-        <Resource name="diretores" list={DiretoresList} />
-        <Resource name="personagens" list={PersonagenList} />
+        <Resource name="filmes" list={FilmesList} edit={EditGuesser} />
+        <Resource name="atores" list={AtoresList} edit={EditGuesser} />
+        <Resource name="diretores" list={DiretoresList} edit={EditGuesser} />
+        <Resource name="personagens" list={PersonagenList} edit={EditGuesser}/>
     </Admin>
   );
 
